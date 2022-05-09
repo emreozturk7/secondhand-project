b3eb92725f37750fbe1c9f0d442bbe9973dd86c6
emreozturk <emre_oztrk_99@hotmail.com>
2022-05-09 06:26:23 +0300

    Detail design yapıldı.

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import '../style/detail.css';
+import Deneme from './Deneme.png';
 
 function Detail() {
   return (
@@ -7,30 +9,33 @@ function Detail() {
       <Header />
       <div className='detail-wrapper'>
         <div className='detail-container'>
-          <div className='detail-image'>Resim</div>
+          <div className='detail-image'>
+            <img src={Deneme} alt="" />
+          </div>
           <div className='detail-info-container'>
-            <div className='detail-title'>
+            <div className='detail-title'>Beli Uzun Trençkot Kareli</div>
+            <div className='main-container'>
               <div className='container'>
-                <div className='title'></div>
-                <div className='name'></div>
-              </div>
-              <div className='container'>
-                <div className='title'></div>
-                <div className='name'></div>
+                <div className='title'>Marka: </div>
+                <div className='name'>Lusi Viton</div>
               </div>
               <div className='container'>
-                <div className='title'></div>
-                <div className='name'></div>
+                <div className='title'>Renk: </div>
+                <div className='name'>Bej Rengi</div>
               </div>
-              <div className='price'></div>
-              <div className='button-container'>
-                <button>Satın Al</button>
-                <button>Teklif Ver</button>
+              <div className='container'>
+                <div className='title'>Kullanım Durumu: </div>
+                <div className='name'>Az Kullanılmış</div>
               </div>
-
-              <div className='explanation'></div>
-              <div className='pharagraph'></div>
             </div>
+            <div className='price'>319.90 TL</div>
+            <div className='button-container'>
+              <button className='buy-button'>Satın Al</button>
+              <button className='offer-button'>Teklif Ver</button>
+            </div>
+
+            <div className='explanation'>Açıklama</div>
+            <div className='pharagraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quasi, eos ipsum impedit magnam cupiditate maxime nihil debitis fugit eaque voluptates voluptatibus dolorem expedita temporibus praesentium suscipit. Perferendis, dolorum? Animi.</div>
           </div>
         </div>
       </div>
